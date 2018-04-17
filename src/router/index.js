b7c176ca6170f508b8d70bf3edca86654c70da4b
TH <757901093@qq.com>
2018-04-17 10:07:33 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,78 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import HomeComponent from "../components/home"
+import HotComponent from "../components/hot"
+import ComingComponent from "../components/coming"
+import MovieComponent from "../components/movie"
+import TheaterComponent from "../components/theater"
+import LoginComponent from "../components/login"
+import RegisterComponent from "../components/register"
+import VewsComponent from "../components/vews"
+import TrailerComponent from "../components/trailer"
+import ToplistComponent from "../components/toplist"
+import ReviewComponent from "../components/review"
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: "hash", //默认值是hash
   routes: [
+    //主页
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/home",
+      component: HomeComponent,
+      children: [{
+          path: "hot",
+          component: HotComponent
+        },
+        {
+          path: "coming",
+          component: ComingComponent
+        }
+      ]
+    },
+
+    //购票&&登录注册
+    {
+      path: "/movie/:id",
+      component: MovieComponent,
+    },
+    {
+      path: "/theater/:id",
+      component: TheaterComponent,
+    },
+    {
+      path: "/login",
+      component: LoginComponent
+    },
+    {
+      path: "/register",
+      component: RegisterComponent
+    },
+
+
+    //发现
+    {
+      path: "/vews",
+      component: VewsComponent
+    },
+    {
+      path: "/trailer",
+      component: TrailerComponent
+    },
+    {
+      path: "/toplist",
+      component: ToplistComponent
+    },
+    {
+      path: "/review",
+      component: ReviewComponent
+    },
+
+
+    {
+      path: "*",
+      redirect: "/home"
     }
   ]
 })
